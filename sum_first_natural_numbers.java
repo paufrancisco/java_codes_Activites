e27ad66d2f6a87fff79de964e0c6c7bda491d5ec
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-08-02 12:17:41 +0800

    5th activity

---
+++ sum_first_natural_numbers.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Comlab
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num; int sum;
+        for ( sum=0,num=1; num<=10; num++ ) {
+            
+        System.out.print(num);
+        sum = sum+num;
+        } 
+        System.out.println(" The Sum is "+ sum);
+    }   
+}
+//Create a Java while loop that calculates the sum of the first (10) natural numbers. Then, print the result.
+    
+    
+
