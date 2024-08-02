3c6adf190b80d0db994079d6370aed21670a18c0
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-08-02 12:19:13 +0800

    6th activity

---
+++ multiple_of_8.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Comlab
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Create a Java do....while loop that prints the multiplication table of 8.
+        int num = 8;
+        do { System.out.println(num);
+                num+=8;
+        }
+            while(num<=80);
+                
+        }
+//Create a Java do....while loop that prints the multiplication table of 8.
+}
+
+    
+    
+
