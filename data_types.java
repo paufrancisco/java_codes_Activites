917d2646ab697dbdd6e61a5b418766dadbe33d4f
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-08-02 12:03:13 +0800

    2nd activity

---
+++ data_types.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package seatwork1;
+
+/**
+ *
+ * @author Comlab
+ */
+public class Seatwork1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String firstName = "Paulo Faustiniel Angelo ";
+        String lastName = "Francisco";
+        char mi	='L';
+        int faveNumber = 27;
+        String faveCartChar = "Kuroko";
+        
+       
+        
+        System.out.println("My name is " + firstName + mi +"." + " "+ lastName + ".");
+        System.out.println(faveNumber + " is my favorite number.");
+        System.out.println("I love " + faveCartChar + "!");
+    }   
+}
