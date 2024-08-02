1663ae85dc24d36db4edfcb7aa13decde07636a2
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-08-02 12:20:17 +0800

    7th activity

---
+++ odd_numbers.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package odd;
+
+/**
+ *
+ * @author Comlab
+ */
+public class Odd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Create a Java for loop that prints the first five (5) odd numbers in descending order.
+   
+    int number = 10;
+
+    for (int i = number - 1; i >= 0; i--) {
+
+        if (i % 2 == 1) {
+            int dec = i;
+            System.out.println(dec);
+        }
+    }
+  }
+//Create a Java for loop that prints the first five (5) odd numbers in descending order.
+}
+  
+    
+    
+    
+    
+
