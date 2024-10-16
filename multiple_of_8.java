b3e27a747c144f95476f924ec8dd0cb3382676c6
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-10-16 17:08:14 +0800

    new practice java code strategy pattern

--- multiple_of_8.java
+++ multiple_of_8.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication3;
-
-/**
- *
- * @author Comlab
- */
-public class JavaApplication3 {
+ 
+public class multiple_of_8 {
 
     /**
      * @param args the command line arguments
