13a3d49bac71c01a1429c07dc83fd10741bf541d
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-10-24 12:54:28 +0800

    update fibonacci 2

--- fibonacci_2.java
+++ fibonacci_2.java
@@ -5,21 +5,21 @@ public class fibonacci_2 {
         int num1 = 0;
         int num2 = 1;
 
-        int numQty = 11;  
         int num3 = 0;
-        
+        int numberQty = 10;
 
-        for (int i = 3; i <= numQty; i++) {
+        int iteration = 0;
+        for (int i = 3; i <= numberQty; i++){
+            
+            //getting sum of num1 and num2 will have num3
+            num3 = num1 + num2;
 
-            num3 = num1 + num2; // sum = 1 , num1 = 0 num2 = 1
-            
-            num1 = num2; // num1 = 1
-            num2 = num3; // num2 = 1
- 
-            
+            //update the num1 and num2
+            num1 = num2;
+            num2 = num3;
+            iteration =i;
         }
-
-        System.out.println("The "+numQty+"th number is "+num3);
-        
+         
+        System.out.println("The "+iteration+"th number: "+ num3);
     }
 }
