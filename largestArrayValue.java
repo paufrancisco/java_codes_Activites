9f36d5aeaf4e4a9d0e71746c1d3a6ab1f6fba9d0
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-10-24 14:49:14 +0800

    find the largest value in array

---
+++ largestArrayValue.java
@@ -0,0 +1,26 @@
+public class largestArrayValue {
+    public static void main(String[] args) {
+        
+        int number [] = {6,2,3,7,5};
+        
+        int largest = number[0];
+        
+
+        for (int i = 1; i < number.length; i++) {
+            
+            if( number[i] > largest){
+
+                largest = number[i];
+                 
+            }
+
+        }
+        System.out.println("The largest number: "+largest);
+    }
+}
+
+
+
+
+
+
